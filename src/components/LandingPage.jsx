3ae37cae03830bfae0678fb50dee3dc631ecdc6d
LWSE13 <leewheeler888@gmail.com>
2024-06-13 02:23:12 +0100

    added buttons and css animations to my landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,8 +3,16 @@ import '../styles/LandingPage.css'
 export default function LandingPage() {
   return (
     <div className="lp-container">
+
       <h1 className="title">Hello World</h1>
       <h3 className="subTitle">Welcome to my portfolio!</h3>
+      <div className='dec-line'></div>
+      <div className='btn-row'>
+      <button className='btn' onClick={() => navigateTo('/about')}>About Me</button>
+      <button className='btn' onClick={() => navigateTo('/portfolio')}>Portfolio</button>
+      <button className='btn' onClick={() => navigateTo('/resume')}>Resume</button>
+      <button className='btn' onClick={() => navigateTo('/contact')}>Contact Me</button>
+      </div>
     </div>
   );
 }
