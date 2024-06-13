3902629c1c334422e6116d5b78a90a4c678896d6
LWSE13 <leewheeler888@gmail.com>
2024-06-13 01:32:32 +0100

    started work on landing page and css

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+import '../styles/LandingPage.css'
+
+export default function LandingPage() {
+  return (
+    <div className="lp-container">
+      <h1 className="title">Hello World</h1>
+      <h3 className="subTitle">Welcome to my portfolio!</h3>
+    </div>
+  );
+}
