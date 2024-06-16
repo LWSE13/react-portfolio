70334440b5cfb643c2b0f529d1dbf0f459b50aa8
LWSE13 <leewheeler888@gmail.com>
2024-06-16 05:49:03 +0100

    changed about me and project css. Added hover screen and desired css to project. Changed structure of projects to accommodate new changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import '../styles/project.css';
+
 function Project({ name, image, deployedUrl, githubUrl }) {
   return (
     <div className="Project">
       <img className='project-img' src={image} alt={name} />
-      <h3>{name}</h3>
-      <a href={deployedUrl} target="_blank" rel="noopener noreferrer">Deployed App</a>
-      <a href={githubUrl} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+      <div className="project-info">
+        <h3 className='project-heading'>{name}</h3>
+        <button 
+          className='btn-project' 
+          onClick={() => window.open(deployedUrl, '_blank', 'noopener,noreferrer')}
+        >
+          Deployed App
+        </button>
+        <button 
+          className='btn-project' 
+          onClick={() => window.open(githubUrl, '_blank', 'noopener,noreferrer')}
+        >
+          GitHub Repo
+        </button>
+      </div>
     </div>
   );
 }
