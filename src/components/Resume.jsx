d1e8dce3100b23012b98585972ba6e09c4eec562
LWSE13 <leewheeler888@gmail.com>
2024-06-17 06:29:37 +0100

    completed work on resume page. Added cv link placeholder and skills list

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -13,7 +13,47 @@ export default function Resume() {
                     <h1 className='title2'>
                         RESUME
                     </h1>
-                    <img className='ctd-img' src="images/c2d2.png" alt="Click To Download" />
+                    <h4 className="cv-link">
+                        Download a copy of <a href="cvpath/cv.pdf" download="YourCVName.pdf">my CV</a> (coming soon)
+                    </h4>
+                    <ul className="resume-skills">
+                        <li>
+                            <h2>EDUCATION</h2>
+                            <p>Full Stack Development Bootcamp - University Of Birmingham</p>
+                            <p>GCSEs - Dudley College</p>
+                        </li>
+                        <div className="line-container">
+                            <div className="dec-line"></div>
+                        </div>
+                        <li>
+                            <h2>EXPERIENCE</h2>
+                            <p>Full Stack Developer - University Of Birmingham</p>
+                            <p>Electrical Installation - Dudley College</p>
+                            <p>Welding - Dudley College</p>
+                        </li>
+                        <div className="line-container">
+                            <div className="dec-line"></div>
+                        </div>
+                        <li>
+                            <h2>SKILLS</h2>
+                            <p>HTML</p>
+                            <p>CSS</p>
+                            <p>Javascript</p>
+                            <p>Node</p>
+                            <p>SQL Databases</p>
+                            <p>MERN Stack</p>
+                        </li>
+                        <div className="line-container">
+                            <div className="dec-line"></div>
+                        </div>
+                        <li>
+                            <h2>INTERESTS</h2>
+                            <p>Python</p>
+                            <p>Rust</p>
+                            <p>C++</p>
+                            <p>AI</p>
+                        </li>
+                    </ul>
                 </div>
                 <Footer />
             </div>
