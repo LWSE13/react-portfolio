0026aa3b5a592919ee9ae9d7a03caece215d92a3
LWSE13 <leewheeler888@gmail.com>
2024-07-14 23:47:12 +0100

    added 2 new projects to my portfolio section

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,6 +2,13 @@
 
 const projectData = [
     {
+        id: 0,
+        name: 'Nanny Stork',
+        image: './images/NS.png',
+        deployedUrl:'https://nanny-stork.onrender.com/',
+        githubUrl:'https://github.com/Shukrim433/nanny-stork'
+    },
+    {
         id: 1,
         name: 'Beefy Stock',
         image: './images/BC.png',
@@ -28,7 +35,14 @@ const projectData = [
         image: './images/IC.png',
         deployedUrl: 'https://lwse13.github.io/I-Clear/',
         githubUrl: 'https://github.com/LWSE13/I-Clear'
-    }
+    },
+    {
+        id: 5,
+        name: 'My Very First Portfolio!',
+        image: './images/MVFP.png',
+        deployedUrl: 'https://lwse13.github.io/Lees-Programming-Portfolio/',
+        githubUrl: 'https://github.com/LWSE13/Lees-Programming-Portfolio?tab=readme-ov-file'
+    },
 ]
 
 export default projectData;
